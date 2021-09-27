66e6edab3ea5ce109ea9f8cb863e06d00835670f
kmonyoncho <kepha.manwa@outlook.com>
2021-09-28 00:07:26 +0300

    ok

--- website/models.py
+++ website/models.py
@@ -25,37 +25,20 @@ class fs_acuserprofiles(db.Model):
     LastName = db.Column(db.String(150))
     Email = db.Column(db.String(150), nullable=False, unique=True)
     PasswordHash = db.Column(db.String(150), nullable=False)
-    SecurityStamp = db.Column(db.String(150))
-    PhoneNumber = db.Column(db.String(150))
+    SecurityStamp = db.Column(db.String(150),default='monyoncho')
+    PhoneNumber = db.Column(db.String(150),default='+254712675612')
     UserName = db.Column(db.String(150), nullable=False, unique=True)
-    AccountLockedOut = db.Column(db.Integer)
-    CountToLock = db.Column(db.Integer)
-    LockoutEnabled = db.Column(db.Integer)
-    AccessFailedCount = db.Column(db.Integer)
-    NextLoginResetPassword = db.Column(db.Integer)
+    is_active = db.Column(db.Boolean, nullable=True, unique=True,default=True)
+    AccountLockedOut = db.Column(db.Integer,default=0)
+    CountToLock = db.Column(db.Integer ,default=3)
+    LockoutEnabled = db.Column(db.Integer,default=1)
+    AccessFailedCount = db.Column(db.Integer,default=1)
+    NextLoginResetPassword = db.Column(db.Integer,default=0)
     LastModifiedBy = db.Column(db.String(150))
     LastModifiedDate = db.Column(
     db.DateTime, default=func.now(), onupdate=func.now())
     CreatedBy = db.Column(db.String(150))
     CreatedDate = db.Column(db.DateTime, default=func.now())
     
-    def __init__(self, EmployeeId, FirstName, LastName, Email, PasswordHash,
-                 SecurityStamp, PhoneNumber, UserName, AccountLockedOut, CountToLock, LockoutEnabled, AccessFailedCount, NextLoginResetPassword, LastModifiedBy, LastModifiedDate, CreatedBy, CreatedDate):
-       EmployeeId.username = EmployeeId
-       self.FirstName = FirstName
-       self.LastName = LastName
-       self.Email = Email
-       self.PasswordHash = PasswordHash
-       self.SecurityStamp = SecurityStamp
-       self.PhoneNumber = PhoneNumber
-       self.AccountLockedOut = AccountLockedOut
-       self.UserName = UserName
-       self.CountToLock = CountToLock
-       self.LockoutEnabled = LockoutEnabled
-       self.AccessFailedCount = AccessFailedCount
-       self.NextLoginResetPassword = NextLoginResetPassword
-       self.LastModifiedBy = LastModifiedBy
-       self.LastModifiedDate = LastModifiedDate
-       self.CreatedBy = CreatedBy
-       self.CreatedDate = CreatedDate       
+      
 
