2a2d3ab273117c3309c36018626b959151e4dbf5
kmonyoncho <kepha.manwa@outlook.com>
2021-09-27 23:21:13 +0300

    ok

--- website/auth.py
+++ website/auth.py
@@ -55,7 +55,7 @@ def register():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+            new_user = fs_acuserprofiles(email=email, first_name=first_name, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
